0777596df631b86a3e57b1ac96b46301d1a1b2dc
awaghat99 <awaghat99@gmail.com>
2023-10-02 12:25:37 +0100

    side bar, no styling, no function

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,21 @@
 import React from "react";
+import compass from "../logos/icons8-compass-50.png";
+import home from "../logos/icons8-home-50.png";
 
 const Sidebar = () => {
-  return <div className="sidebar"></div>;
+  return (
+    <div className="sidebar">
+      <h3>Instagram</h3>
+      <div className="home button">
+        <img src={home} alt={home} />
+        <h3>Home</h3>
+      </div>
+      <div className="explore button">
+        <img src={compass} alt="compass" />
+        <h3>Explore</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
